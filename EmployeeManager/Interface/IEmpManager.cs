00cf972bfc80c97201ad00f1767ee4ab90aab789
Raghav <raghavwandre1234@gmail.com>
2022-01-03 12:01:19 +0530

    Implemented Register, Login,Reset,Forget

---
+++ EmployeeManager/Interface/IEmpManager.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManager.Interface
+{
+    public interface IEmpManager
+    {
+        string EmployeeDetails(EmployeeDetails employeeDetails);
+
+        string Delete(string deleteData);
+
+        string Edit(string edit, string gender, string department, int salary, int startDate);
+
+
+    }
+}
---
+++ EmployeeNote/Controller/EmpController.cs
@@ -0,0 +1,93 @@
+﻿using EmployeeManager.Interface;
+using EmployeeModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeNote.Controller
+{
+    [ApiController]
+    [Route("api/[Empcontroller]")]
+    public class EmpController:ControllerBase
+    {
+        private readonly IEmpManager Empmanager;
+
+        public EmpController(IEmpManager Empmanager)
+        {
+            this.Empmanager = Empmanager;
+        }
+
+
+        [HttpGet]
+        [Route("api/EmployeeDetails")]
+        public IActionResult EmployeeDetails([FromBody] EmployeeDetails EmployeeDetails)
+        {
+            try
+            {
+                string message = this.Empmanager.EmployeeDetails(EmployeeDetails);
+                if (message.Equals("Employee Details Added Successfully"))
+                {
+                    return this.Ok(new { Status = true, Message = message });
+                }
+                else
+                {
+                    return this.BadRequest(new { Status = false, Message = message });
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new { Status = false, ex.Message });
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("deleteData")]
+        public IActionResult Delete(string deleteData)
+        {
+            try
+            {
+                string message = this.Empmanager.Delete(deleteData);
+                if (message.Equals("Data Deleted Successfully"))
+                {
+                    return this.Ok(new { Status = true, Message = message });
+                }
+                else
+                {
+                    return this.BadRequest(new { Status = false, Message = message });
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new { Status = false, ex.Message });
+            }
+        }
+
+
+        [HttpPut]
+        [Route("editData")]
+        public IActionResult EditDetails(string FullName, string Gender, string Department, int Salary, int StartDate)
+        {
+            try
+            {
+                string message = this.Empmanager.Edit(FullName, Gender, Department,Salary, StartDate);
+                if (message.Equals("Information Edited Successfully"))
+                {
+                    return this.Ok(new { Status = true, Message = message });
+                }
+                else
+                {
+                    return this.BadRequest(new { Status = false, Message = message });
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new { Status = false, ex.Message });
+            }
+        }
+
+
+    }
+}
